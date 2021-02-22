ee26027d3eeec760a58f4b14280cf0de48fe1cec
João Aristeu <jasc.eai18@uea.edu.br>
2021-02-22 14:56:05 -0400

    Atualizando código da aula teste

--- LDR_LED/LDR_LED.ino
+++ LDR_LED/LDR_LED.ino
@@ -11,14 +11,15 @@
 //declara a variável que receber a leitura analógica
 int valor_LDR = 0;
 //declara o pino do LED como pino digital 8
-const int pin_LED = 8;
+const int pin_LED = 5;
+const int pin_sensor = 0;
 
 void setup()
 {
   //Inicializa a serial
  Serial.begin(9600);
  //define A0 como entrada
-  pinMode(A0, INPUT);
+  pinMode(pin_sensor, INPUT);
   //define o pino do LED como saída
   pinMode(pin_LED, OUTPUT);
 }
@@ -26,12 +27,12 @@ void setup()
 void loop()
 {
   // Atribui a leitura de A0 à variável
-  valor_LDR = analogRead(A0);
+  valor_LDR = analogRead(pin_sensor);
   // Escreve o valor de lido em A0 no monitor Serial
   Serial.print("Leitura do LDR: ");
   Serial.println(valor_LDR);
   // Condição que verifica se valor_LDR é maior que valor da luminosidade local
-  if (valor_LDR > 700) { //configure o valor "700" conforme a luminosidade de onde você está.
+  if (valor_LDR < 400) { //configure o valor "700" conforme a luminosidade de onde você está.
     // Acende o LED
     digitalWrite(pin_LED, HIGH);
     // Condição contrária a do "if'
